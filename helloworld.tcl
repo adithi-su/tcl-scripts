01be02e030fb338a0fbbed6d4008e2a006dc6750
Adithi <63908022+adithi-su@users.noreply.github.com>
2022-03-11 08:22:32 +0530

    Create helloworld.tcl

---
+++ helloworld.tcl
@@ -0,0 +1,11 @@
+puts "Hello, World - In quotes"    ;
+# This is a comment 
+puts {Hello, World - In Braces}
+puts {Bad comment syntax example}   # *Error* - there is no semicolon!
+
+puts "This is line 1"; puts "this is line 2"
+
+puts "Hello, World; - With  a semicolon inside the quotes"
+
+# Words don't need to be quoted unless they contain white space:
+puts HelloWorld
